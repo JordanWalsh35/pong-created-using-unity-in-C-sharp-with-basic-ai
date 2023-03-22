04fcded03892eb414120cf28acaf3b4aa00f75e9
Jordan Walsh <111191608+JordanWalsh35@users.noreply.github.com>
2023-03-22 08:50:06 +0000

    Add files via upload

---
+++ Assets/Resources/Scripts/MoveRacket1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket1 : MonoBehaviour {
+
+	public float movementSpeed;
+
+	private void FixedUpdate()
+	{
+		float v = Input.GetAxisRaw("Vertical");
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2(0, v) * movementSpeed;
+
+	}
+
+}
---
+++ Assets/Resources/Scripts/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour 
+{
+	public void PlayGame()
+	{
+		// Debug.Log("Button was pressed");
+
+		SceneManager.LoadScene("Game");
+	}
+}
